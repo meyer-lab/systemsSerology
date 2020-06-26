a44d106c8a5f216a0f9eb02263e95e4f3ba93fb7
Aaron Meyer <git@ameyer.me>
2020-06-25 17:34:26 -0700

    Test should run now.

--- test/testR2X.jl
+++ test/testR2X.jl
@@ -1,11 +1,15 @@
-"Test monotonically increasing R2X with more CP components"
-function test_CP_R2X()
-    orig = createCube()
+
+@testset "Test monotonically increasing R2X with more CP components" begin
+    orig = systemsSerology.createCube()
     arr = zeros(10)
+
     for i = 1:10
-        output = CP_decomposition(i)
-        reconstruct = cp_reconstruct(output)
-        arr[i] = r2x(reconstruct, orig)
+        output = systemsSerology.CP_decomposition(i)
+        reconstruct = systemsSerology.cp_reconstruct(output)
+        arr[i] = systemsSerology.r2x(reconstruct, orig)
+
+        @assert arr[i] <= 1.0
+
         if i > 1
             @assert arr[i] > arr[i-1]
         end
