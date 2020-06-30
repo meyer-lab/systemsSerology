d899162f7216b45e9e2d719252caf6ebe245727b
Madeleine <murphym21@g.ucla.edu>
2020-06-30 14:36:30 -0700

    evaluate missingness function

--- syserol/impute.py
+++ syserol/impute.py
@@ -31,3 +31,43 @@ def evalMissing(nComp = 1, numSample = 100):
             break
 
     return orig, recon
+
+def evaluate_missing():
+    Cube, GlyCube = createCube()
+    orig = Cube #copy of original for comparison
+    indices = list()
+    #replace 200 real values with missing values
+    for num in range(200):
+        i = 0
+        j = 0
+        k = 0
+        while True:
+            i = rd.randint(0,180) #pick a subject
+            j = rd.randint(0,21) #pick a detection 
+            k = rd.randint(0,40) #pick an antigen 
+            if not np.isnan(Cube[i,j,k]):
+                break
+
+        Cube[i,j,k] = np.nan #make that value a NaN    
+        indices.append((i,j,k)) #store its index
+    
+    
+    #check differences between original and recon values for different number of components
+    Averages = list()
+    Sums = list()
+    for comp in np.arange(1,10):
+        recon = impute(Cube, comp)
+        Diff = list()
+        for ind in range(len(indices)):
+            index = indices[ind]
+            origVal = orig[index]
+            reconVal = recon[index]
+            err = abs(reconVal - origVal) #find difference between values
+            Diff.append(err)
+        Avg = mean(Diff)
+        Sum = sum(Diff)
+        print("The average difference for", comp, "components is:", Avg, "and the Sum is:", Sum)
+        Averages.append(Avg)
+        Sums.append(Sum)
+
+    return Averages, Sums
