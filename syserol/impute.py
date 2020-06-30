cb849cb281a098e910afda1c9ecbe6a07a88818d
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-06-30 16:35:29 -0700

    Update impute.py

--- syserol/impute.py
+++ syserol/impute.py
@@ -7,69 +7,43 @@ from .dataImport import createCube
 from .tensor import perform_decomposition, impute, R2X
 
 
-
-def evalMissing(nComp = 1, numSample = 100):
+def evalMissing(cube, nComp = 1, numSample = 100):
     """ Evaluate how well factorization imputes missing values. """
-    cube = createCube()
-
+    cube = np.copy(cube)
     orig = []
     recon = []
 
+    indices = list()
     idxs = np.argwhere(np.isfinite(cube))
 
     for ii in range(numSample):
         i, j, k = idxs[np.random.choice(idxs.shape[0], 1)][0]
 
+        indices.append((i, j, k))
         orig.append(cube[i, j, k])
-        cubeTemp = np.copy(cube)
-        cubeTemp[i, j, k] = np.nan
+        cube[i, j, k] = np.nan
 
-        tensorR = impute(cubeTemp, nComp)
+    tensorR = impute(cube, nComp)
+    
+    for ii in range(indices):
+        recon.append(tensorR[indices[ii][0], indices[ii][1], indices[ii][2]])
 
-        recon.append(tensorR[i, j, k])
-        print(len(recon))
-        print(np.corrcoef(orig, recon)[0, 1])
+    return np.array(orig), np.array(recon)
 
-        if len(recon) > 100:
-            break
-
-    return orig, recon
 
 def evaluate_missing():
     Cube, GlyCube = createCube()
-    orig = Cube #copy of original for comparison
-    indices = list()
-    #replace 200 real values with missing values
-    for num in range(200):
-        i = 0
-        j = 0
-        k = 0
-        while True:
-            i = rd.randint(0,180) #pick a subject
-            j = rd.randint(0,21) #pick a detection 
-            k = rd.randint(0,40) #pick an antigen 
-            if not np.isnan(Cube[i,j,k]):
-                break
-
-        Cube[i,j,k] = np.nan #make that value a NaN    
-        indices.append((i,j,k)) #store its index
-    
     
     #check differences between original and recon values for different number of components
     Averages = list()
     Sums = list()
     for comp in np.arange(1,10):
-        recon = impute(Cube, comp)
-        Diff = list()
-        for ind in range(len(indices)):
-            index = indices[ind]
-            origVal = orig[index]
-            reconVal = recon[index]
-            err = abs(reconVal - origVal) #find difference between values
-            Diff.append(err)
-        Avg = mean(Diff)
+        orig, recon = evalMissing(Cube, nComp = comp, numSample = 100)
+
+        Diff = np.absolute(orig - recon)
+        Avg = np.mean(Diff)
         Sum = sum(Diff)
-        print("The average difference for", comp, "components is:", Avg, "and the Sum is:", Sum)
+        print(f"The average difference for {comp} components is: {Avg} and the Sum is: {Sum}")
         Averages.append(Avg)
         Sums.append(Sum)
 
