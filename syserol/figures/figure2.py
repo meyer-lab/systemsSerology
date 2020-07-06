38d1e471f572b4478953582f119561c3ce1ba82c
Madeleine <murphym21@g.ucla.edu>
2020-07-06 15:55:07 -0700

    function prediction figures

--- syserol/figures/figure2.py
+++ syserol/figures/figure2.py
@@ -4,11 +4,17 @@ This creates Figure 2.
 
 from .common import subplotLabel, getSetup
 
-
-def makeFigure():
-    """Get a list of the axis objects and create a figure"""
+def makeFigure(function):
+     """Make Alter Model Prediction Figure for given Function"""
     # Get list of axis objects
     ax, f = getSetup((10, 10), (1, 1))
+    
+    Measured, Y_pred, corr = function_elastic_net(function)
+    ax[0].scatter(Measured, Y_pred)
+    ax[0].set_ylabel("Predicted", fontsize = 12)
+    ax[0].set_xlabel("Measured", fontsize = 12)
+    ax[0].set_title(f"{function} Predictability", fontsize = 15)
+    ax[0].plot([Y_pred.min(), Y_pred.max()], [Y_pred.min(), Y_pred.max()], 'k--', lw=4)
 
     # Add subplot labels
     subplotLabel(ax)
