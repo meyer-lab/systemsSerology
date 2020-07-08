85a74ca98c2d52031d131e2b2b7b50897e477465
Madeleine <murphym21@g.ucla.edu>
2020-07-08 15:36:07 -0700

    add missing value data figures, plus fix eval function

--- syserol/figures/figure3.py
+++ syserol/figures/figure3.py
@@ -1,15 +1,29 @@
 """
 This creates Figure 3.
 """
-
+import numpy as np
 from .common import subplotLabel, getSetup
+from syserol.impute import evaluate_missing
 
 
 def makeFigure():
-    """Get a list of the axis objects and create a figure"""
+    """ Evaluate Handling of Missing Values in Decomposition """
     # Get list of axis objects
-    ax, f = getSetup((10, 10), (1, 1))
+    ax, f = getSetup((10, 10), (2, 1))
+    
+    Averages, Sums = evaluate_missing()
+    
+    x = np.arange(1,10)
+    ax[0].plot(x, Averages)
+    ax[0].set_ylabel("Difference", fontsize = 12)
+    ax[0].set_xlabel("Component", fontsize = 12)
+    ax[0].set_title("Average Difference Between 100 Original and Reconstructed Values", fontsize = 15)
 
+    ax[1].plot(x, Sums)
+    ax[1].set_ylabel("Sum", fontsize = 12)
+    ax[1].set_xlabel("Component", fontsize = 12)
+    ax[1].set_title("Sum of Differences between 100 Original and Reconstructed Values", fontsize = 15)
+    
     # Add subplot labels
     subplotLabel(ax)
 
--- syserol/impute.py
+++ syserol/impute.py
@@ -11,11 +11,11 @@ def evalMissing(cube, nComp=1, numSample=100):
     orig = []
 
     indices = list()
-    idxs = np.argwhere(np.isfinite(cube))
 
     for _ in range(numSample):
+        idxs = np.argwhere(np.isfinite(cube))
         i, j, k = idxs[np.random.choice(idxs.shape[0], 1)][0]
-
+        
         indices.append((i, j, k))
         orig.append(cube[i, j, k])
         cube[i, j, k] = np.nan
@@ -24,7 +24,6 @@ def evalMissing(cube, nComp=1, numSample=100):
     recon = [tensorR[indx[0], indx[1], indx[2]] for indx in indices]
 
     return np.array(orig), np.array(recon)
-
 
 def evaluate_missing():
     """ check differences between original and recon values for different number of components. """
