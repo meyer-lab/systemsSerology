fa1e00fa9f165af899311cb458dff92602152b39
Aaron Meyer <git@ameyer.me>
2020-07-11 09:54:09 -0700

    Simplify plot.

--- syserol/figures/figure3.py
+++ syserol/figures/figure3.py
@@ -9,15 +9,15 @@ from syserol.impute import evaluate_missing
 def makeFigure():
     """ Evaluate Handling of Missing Values in Decomposition """
     # Get list of axis objects
-    ax, f = getSetup((10, 10), (1, 1))
+    ax, f = getSetup((6, 6), (1, 1))
     
     Sumsqs = evaluate_missing()
     
     x = np.arange(1,10)
     ax[0].plot(x, Sumsqs)
-    ax[0].set_ylabel("Sum Squared Difference", fontsize = 12)
-    ax[0].set_xlabel("Component", fontsize = 12)
-    ax[0].set_title("Sum of Squared Difference Between 100 Original and Reconstructed Values", fontsize = 15)
+    ax[0].set_ylabel("Normalized Reconstruction Error")
+    ax[0].set_xlabel("# Components")
+    ax[0].set_ylim(bottom=0.0)
   
     # Add subplot labels
     subplotLabel(ax)
