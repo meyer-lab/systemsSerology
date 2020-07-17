ee5aa5fcb33cfe3246fa6f8ea6f3f8f39e2828b1
Madeleine <murphym21@g.ucla.edu>
2020-07-17 13:58:22 -0700

    can add components 3 & 4

--- syserol/figures/figure6.py
+++ syserol/figures/figure6.py
@@ -39,4 +39,31 @@ def makeFigure():
         ax[i].set_xlabel("Component 1")
     subplotLabel(ax)
     
+    # Components 3&4
+    ax, f = getSetup((10, 10), (3, 2))
+    subjects = np.squeeze(tensorFac.factors[0])
+    ax[0].plot(subjects[:, 2], subjects[:, 3], '.')
+    ax[0].set_title("Tensor Fac: Subjects", fontsize = 15)
+
+    receptors = np.squeeze(tensorFac.factors[1])
+    ax[1].plot(receptors[:, 2], receptors[:, 3], '.')
+    ax[1].set_title("Tensor Fac: Receptors", fontsize = 15)
+
+    antigens = np.squeeze(tensorFac.factors[2])
+    ax[2].plot(antigens[:, 2], antigens[:, 3], '.')
+    ax[2].set_title("Tensor Fac: Antigens", fontsize = 15)
+
+    subjects = np.squeeze(matrixFac.factors[0])
+    ax[3].plot(subjects[:, 2], subjects[:, 3], '.')
+    ax[3].set_title("Matrix Fac: Subjects", fontsize = 15)
+
+    glyc = np.squeeze(matrixFac.factors[1])
+    ax[4].plot(glyc[:, 2], glyc[:, 3], '.')
+    ax[4].set_title("Matrix Fac: Glycans, Functions, and Classifications", fontsize = 15)
+
+    for i in range(size):
+        ax[i].set_ylabel("Component 4")
+        ax[i].set_xlabel("Component 3")
+    subplotLabel(ax)
+    
     return f
