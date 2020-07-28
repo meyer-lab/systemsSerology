20192b8adca4016d87db90dc3525eda17902e52b
Madeleine <murphym21@g.ucla.edu>
2020-07-28 16:44:29 -0700

    trying to see figuer7 build on jenkins, should work

--- syserol/figures/figure7.py
+++ syserol/figures/figure7.py
@@ -4,33 +4,28 @@ This creates Figure 7.
 import matplotlib
 import numpy as np
 from syserol.figures.common import subplotLabel, getSetup
-from syserol.model import cross_validation, evaluate_diff
+from syserol.model import cross_validation
+from sklearn.metrics import r2_score
 
 def makeFigure():
     """ Analyze Prediction Accuracy of 10 Fold Cross Validation Strategy"""
-    x, y, _ = cross_validation()
-    Sumsqs, Avg = evaluate_diff()
-    ax, f = getSetup((10, 10), (2, 1))
-    
-    # Plot original values vs CMTF reconstructed values (after original->NaN)
-    ax[0].scatter(x, y)
-    ax[0].set_ylabel("Reconstructed Values", fontsize=12)
-    ax[0].set_xlabel("Original Values", fontsize=12)
-    ax[0].set_title("Comparison of Original and Reconstructed Values from 10 Fold Cross Validation Methods", fontsize=15)
-    idx = np.isfinite(x) & np.isfinite(y)
-    m, b = np.polyfit(x[idx], y[idx], 1) # line of best fit
-    ax[0].plot(x, m*x + b, 'k--', color='black')
-
-    # Plot the Difference Squared for each pair of original&reconstructed values
-    # Overlay Average Difference Squared across all pairs
-    ax[1].plot(Sumsqs)
-    ax[1].axhline(y=Avg, linestyle='-', color="red", label="mean")
-    ax[1].set_xlabel("# Values Predicted", fontsize=12)
-    ax[1].set_ylabel("Sum of Squared Difference", fontsize=12)
-    ax[1].set_title("Sum of Squared Difference between Original and Predicted Values from 10 Fold Cross Validation", fontsize=15)
-    ax[1].legend(prop={'size': 15})
-    ax[1].set_ylim(bottom=0.0)
-    ax[1].set_xlim(0.0, len(Sumsqs))
+    ax, f = getSetup((10, 10), (3, 2))
+    matrix = cross_validation()
+    functions = ["ADCD", "ADCC", "ADNP", "CD107a", "IFNy", "MIP1b"]
+    # Plot Actual vs. Predicted Values for each Function
+    for i in np.arange(6):
+        x = matrix[0:181, i]
+        y = matrix[0:181, i+6]
+        ax[i].scatter(x, y)
+        ax[i].set_xlabel("Actual Values", fontsize=12)
+        ax[i].set_ylabel("Predicted Values", fontsize=12)
+        idx = np.isfinite(x)
+        r2 = r2_score(x[idx], y[idx]) # coefficient of determination
+        m, b = np.polyfit(x[idx], y[idx], 1) # line of best fit
+        ax[i].plot(x, m*x + b, 'k--', color="red")
+        ax[i].text(1, 1, fr"$R^2$ Score: {round(r2, 3)}", {"color": "red", "fontsize": 10}, horizontalalignment="right",      
+                   verticalalignment="bottom", transform=ax[i].transAxes)
+        ax[i].set_title(functions[i], fontsize=15)
 
     subplotLabel(ax)
     return f
