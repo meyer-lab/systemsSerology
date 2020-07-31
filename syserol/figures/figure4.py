a8aaf8b590e663725c2b5b6668dd9d7a58cfcb36
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-07-31 08:20:43 -0700

    Remove CMTF EN (#35)
    Remove CMTF-Elastic Net code, since we'll just perform prediction through the factorization itself.
    
    Co-authored-by: Aaron Meyer <git@ameyer.me>

--- syserol/figures/figure4.py
+++ syserol/figures/figure4.py
@@ -2,22 +2,13 @@
 This creates Figure 4.
 """
 
-from syserol.model import CMTF_elastic_function_predictions
 from .common import subplotLabel, getSetup
 
 def makeFigure():
-    """Function Prediction Figures using Our Model with Factorization"""
+    """ Builds the figure. """
     # Get list of axis objects
     ax, f = getSetup((10, 10), (2, 3))
 
-    functions = ['ADCD', 'ADCC', 'ADNP', 'CD107a', 'IFNy', 'MIP1b']
-    for i, name in enumerate(functions):
-        Measured, Y_pred, _ = CMTF_elastic_function_predictions(name)
-        ax[i].scatter(Measured, Y_pred)
-        ax[i].set_ylabel("Predicted", fontsize=12)
-        ax[i].set_xlabel("Measured", fontsize=12)
-        ax[i].set_title(f"{name} Predictability", fontsize=15)
-        ax[i].plot([Y_pred.min(), Y_pred.max()], [Y_pred.min(), Y_pred.max()], 'k--', lw=4)
-        subplotLabel(ax)
+    subplotLabel(ax)
 
     return f
--- syserol/model.py
+++ syserol/model.py
@@ -1,85 +1,11 @@
 """ Regression methods using Factorized Data. """
-import pandas as pd
 import numpy as np
 import tensorly as tl
 from sklearn.model_selection import KFold
-from sklearn.linear_model import ElasticNetCV, ElasticNet, LogisticRegressionCV
-from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import r2_score, confusion_matrix
-from scipy.stats import zscore
+from sklearn.metrics import r2_score
 from tensorly.kruskal_tensor import kruskal_to_tensor
 from syserol.tensor import perform_CMTF
-from syserol.dataImport import createCube, importFunction, load_file, importGlycan
-
-
-def CMTF_elastic_function_predictions(function="ADCD"):
-    """ Predict Function using CMTF Decomposed Antigen Data"""
-    # import
-    cube, glyCube = createCube()
-    tensorFac, _, _ = perform_CMTF(cube, glyCube, 16)
-    func, _ = importFunction()
-
-    # arrange
-    df = pd.DataFrame(tensorFac[1][0])  # subjects x components matrix
-    df = df.join(func, how="inner")
-    df = df.dropna()
-    df_func = df[["ADCD", "ADCC", "ADNP", "CD107a", "IFNy", "MIP1b"]]
-    df_variables = df.drop(["subject", "ADCD", "ADCC", "ADNP", "CD107a", "IFNy", "MIP1b"], axis=1)
-
-    # Regression
-    X = df_variables
-    Y = df_func[function]
-    regr = ElasticNetCV(normalize=True, max_iter=10000)
-    model = regr.fit(X, Y)
-    Y_pred = cross_val_predict(ElasticNet(alpha=regr.alpha_, normalize=True, max_iter=10000), X, Y, cv=10)
-
-    print(model.coef_)
-    print(np.sqrt(r2_score(Y, Y_pred)))
-
-    return Y, Y_pred, np.sqrt(r2_score(Y, Y_pred))
-
-
-def CMTF_logistic_subject_predictions():
-    """ Predict Subject Classifications using CMTF Decomposed Antigen Data"""
-    cube, glyCube = createCube()
-    tensorFac, _, _ = perform_CMTF(cube, glyCube, 16)
-
-    # Assemble data
-    df = pd.DataFrame(tensorFac[1][0])  # subjects x components matrix
-    subj = load_file("meta-subjects")
-    df = df.join(subj, how="inner")
-    df = df.dropna()
-
-    # Subset, Z score
-    df_class = df[["class.cp", "class.nv"]]
-    df_variables = df.drop(["subject", "class.etuv", "class.cp", "class.nv"], axis=1)
-    df_variables = df_variables.apply(zscore)
-
-    # Predict Controller vs. Progressor
-    Y1 = df_class["class.cp"]
-    Y1 = (Y1 == "controller").astype(int)  # controllers are 1s, progressors are 0s
-    X1 = df_variables
-
-    Y_pred1 = cross_val_predict(LogisticRegressionCV(max_iter=10000), X1, Y1)
-    model1 = LogisticRegressionCV().fit(X1, Y1)
-
-    print(model1.coef_)
-    accuracyCvP = confusion_matrix(Y1, Y_pred1)
-    print(f"Confusion Matrix Controller vs. Progressor: {accuracyCvP} \n")
-
-    # Predict Viremic vs. Nonviremic
-    Y2 = df_class["class.nv"]
-    Y2 = (Y2 == "viremic").astype(int)  # viremic = 1, nonviremic = 0
-    X2 = df_variables
-
-    Y_pred2 = cross_val_predict(LogisticRegressionCV(max_iter=10000), X2, Y2)
-    model2 = LogisticRegressionCV().fit(X2, Y2)
-
-    print(model2.coef_)
-    accuracyVvN = confusion_matrix(Y2, Y_pred2)
-    print(f"Confusion Matrix Viremic vs. Nonviremic: {accuracyVvN} \n")
-
-    return accuracyCvP, accuracyVvN
+from syserol.dataImport import createCube, importFunction, importGlycan
 
 
 def test_predictions(function="ADCD"):
