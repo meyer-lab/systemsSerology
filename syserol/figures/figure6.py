4aac3345f1539eb059a7f6400e4d3e6697c0662d
Madeleine Murphy <47797938+murphymadeleine21@users.noreply.github.com>
2020-08-04 14:51:46 -0700

    fix repetition in figure 6 (#38)

--- syserol/figures/figure6.py
+++ syserol/figures/figure6.py
@@ -12,8 +12,8 @@ def makeFigure():
     cube, glyCube = createCube()
     tensorFac, matrixFac, _ = perform_CMTF(cube, glyCube, 10)
     
-    size = 5
-    ax, f = getSetup((10, 10), (5, 2))
+    size = 4
+    ax, f = getSetup((10, 10), (4, 2))
 
     subjects = np.squeeze(tensorFac.factors[0])
     ax[0].plot(subjects[:, 0], subjects[:, 1], '.')
@@ -27,13 +27,9 @@ def makeFigure():
     ax[2].plot(antigens[:, 0], antigens[:, 1], '.')
     ax[2].set_title("Tensor Fac: Antigens", fontsize=15)
 
-    subjects = np.squeeze(matrixFac.factors[0])
-    ax[3].plot(subjects[:, 0], subjects[:, 1], '.')
-    ax[3].set_title("Matrix Fac: Subjects", fontsize=15)
-
     glyc = np.squeeze(matrixFac.factors[1])
-    ax[4].plot(glyc[:, 0], glyc[:, 1], '.')
-    ax[4].set_title("Matrix Fac: Glycans, Functions, and Classifications", fontsize=15)
+    ax[3].plot(glyc[:, 0], glyc[:, 1], '.')
+    ax[3].set_title("Matrix Fac: Glycans, Functions, and Classifications", fontsize=15)
 
     for i in range(size):
         ax[i].set_ylabel("Component 2")
@@ -41,26 +37,22 @@ def makeFigure():
     
     # Components 3&4
     subjects = np.squeeze(tensorFac.factors[0])
-    ax[5].plot(subjects[:, 2], subjects[:, 3], '.')
-    ax[5].set_title("Tensor Fac: Subjects", fontsize=15)
+    ax[4].plot(subjects[:, 2], subjects[:, 3], '.')
+    ax[4].set_title("Tensor Fac: Subjects", fontsize=15)
 
     receptors = np.squeeze(tensorFac.factors[1])
-    ax[6].plot(receptors[:, 2], receptors[:, 3], '.')
-    ax[6].set_title("Tensor Fac: Receptors", fontsize=15)
+    ax[5].plot(receptors[:, 2], receptors[:, 3], '.')
+    ax[5].set_title("Tensor Fac: Receptors", fontsize=15)
 
     antigens = np.squeeze(tensorFac.factors[2])
-    ax[7].plot(antigens[:, 2], antigens[:, 3], '.')
-    ax[7].set_title("Tensor Fac: Antigens", fontsize=15)
-
-    subjects = np.squeeze(matrixFac.factors[0])
-    ax[8].plot(subjects[:, 2], subjects[:, 3], '.')
-    ax[8].set_title("Matrix Fac: Subjects", fontsize=15)
+    ax[6].plot(antigens[:, 2], antigens[:, 3], '.')
+    ax[6].set_title("Tensor Fac: Antigens", fontsize=15)
 
     glyc = np.squeeze(matrixFac.factors[1])
-    ax[9].plot(glyc[:, 2], glyc[:, 3], '.')
-    ax[9].set_title("Matrix Fac: Glycans, Functions, and Classifications", fontsize=15)
+    ax[7].plot(glyc[:, 2], glyc[:, 3], '.')
+    ax[7].set_title("Matrix Fac: Glycans, Functions, and Classifications", fontsize=15)
 
-    for i in np.arange(5,10):
+    for i in np.arange(4,8):
         ax[i].set_ylabel("Component 4")
         ax[i].set_xlabel("Component 3")
     subplotLabel(ax)
