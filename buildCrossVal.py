1c9a8a138e5aee9108b2e6dcec17221f03ac9d48
Madeleine Murphy <47797938+murphymadeleine21@users.noreply.github.com>
2020-08-24 10:56:09 -0700

    Cross Validation Cache (#52)

--- buildCrossVal.py
+++ buildCrossVal.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
 
 import pickle
 from syserol.model import Function_Prediction_10FoldCV
 
 if __name__ == "__main__":
-    matrix = Function_Prediction_10FoldCV(components=10)
-    pickle.dumb(matrix, open("crossValidationMatrix.p", "wb"))
+    matrix = Function_Prediction_10FoldCV(10)
+    pickle.dump(matrix, open("crossValidationMatrix.p", "wb") )
