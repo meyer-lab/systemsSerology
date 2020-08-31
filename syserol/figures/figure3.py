4294b38dda6886f09b80f7beac8e265cafe236c2
Madeleine <murphym21@g.ucla.edu>
2020-08-31 14:24:19 -0700

    suplementary figures

---
+++ syserol/figures/figure3.py
@@ -0,0 +1,221 @@
+"""
+This creates Figure 3 for the Paper.
+"""
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from syserol.tensor import perform_CMTF
+from syserol.dataImport import createCube, getAxes, load_file
+from .common import getSetup
+
+
+def makeFigure():
+    """ Generate Figure 3 for Paper, Showing Better Interpretation of All Data from Decomposed Tensor"""
+    ax, f = getSetup((8, 8), (3, 4))
+
+    cube, glyCube = createCube()
+    tensorFac, matrixFac, _, _ = perform_CMTF(cube, glyCube, 6)
+
+    # Gather tensor data matrices
+    subjects = np.squeeze(tensorFac.factors[0])
+    receptors = np.squeeze(tensorFac.factors[1])
+    antigens = np.squeeze(tensorFac.factors[2])
+    glyc = np.squeeze(matrixFac.factors[1])
+
+    # Gather grouping info
+    glycaninf = load_file("meta-glycans")
+    glycaninf = glycaninf.replace(
+        to_replace=["false", "b", "f", "g1", "g2", "g0", "s"],
+        value=["", "B", "F", "G1", "G2", "G0", "S"],
+    )
+    for i in np.arange(0, len(glycaninf)):
+        if "S1" in glycaninf.iloc[i, 0]:
+            glycaninf.iloc[i, 2] = "S1"
+        if "S2" in glycaninf.iloc[i, 0]:
+            glycaninf.iloc[i, 2] = "S2"
+    glycaninf["FB"] = glycaninf["f"] + glycaninf["b"]
+    glycaninf["GS"] = glycaninf["g"] + glycaninf["s"]
+    glycaninf["FB"] = glycaninf["FB"].replace(to_replace=[np.nan, ""], value=["Total", "No F or B"])
+    glycaninf.loc[19:24, "GS"] = glycaninf.loc[19:24, "glycan"]
+    _, detections, antigen = getAxes()
+    subjinfo = load_file("meta-subjects")
+
+    index = [0, 2, 4]
+    place = [0, 4, 8]
+    # Build Figure
+    for i, j in zip(index, place):
+        # Subjects
+        values1 = subjects[:, i]
+        values2 = subjects[:, i + 1]
+        data = {
+            f"Component {i+1}": values1,
+            f"Component {i+2}": values2,
+            "Groups": subjinfo["class.etuv"],
+        }
+        df = pd.DataFrame(data)
+        xmax = np.amax(np.absolute(values1))
+        ymax = np.amax(np.absolute(values2))
+        a = sns.scatterplot(
+            x=f"Component {i+1}",
+            y=f"Component {i+2}",
+            hue="Groups",
+            data=df,
+            palette="Set1",
+            legend="brief" if j == 4 else False,
+            ax=ax[j],
+        )
+        a.legend(loc="center left", bbox_to_anchor=(1, 0.5), ncol=1)
+        a.set_xlim(-xmax, xmax)
+        a.set_ylim(-ymax, ymax)
+
+        # Detections
+        values1 = receptors[:, i]
+        values2 = receptors[:, i + 1]
+        data = {
+            f"Component {i+1}": values1,
+            f"Component {i+2}": values2,
+            "Receptor": detections,
+        }
+        df = pd.DataFrame(data)
+        xmax = np.amax(np.absolute(values1))
+        ymax = np.amax(np.absolute(values2))
+        markers = (
+            "o",
+            "X",
+            "X",
+            "X",
+            "^",
+            "D",
+            "D",
+            "D",
+            "D",
+            "D",
+            "D",
+            "<",
+            ">",
+            "8",
+            "s",
+            "P",
+            "P",
+            "P",
+            "P",
+            "P",
+            "p",
+            "d",
+        )
+        b = sns.scatterplot(
+            x=f"Component {i+1}",
+            y=f"Component {i+2}",
+            hue="Receptor",
+            style="Receptor",
+            markers=markers,
+            data=df,
+            palette="Set2",
+            legend="brief" if j == 4 else False,
+            ax=ax[j + 1],
+        )
+        b.legend(loc="center left", bbox_to_anchor=(1, 0.5), ncol=1)
+        b.set_xlim(-xmax, xmax)
+        b.set_ylim(-ymax, ymax)
+
+        # Antigens
+        values1 = antigens[:, i]
+        values2 = antigens[:, i + 1]
+        data = {
+            f"Component {i+1}": values1,
+            f"Component {i+2}": values2,
+            "Antigens": antigen,
+        }
+        df = pd.DataFrame(data)
+        xmax = np.amax(np.absolute(values1))
+        ymax = np.amax(np.absolute(values2))
+        markers = (
+            "o",
+            "v",
+            "^",
+            "<",
+            ">",
+            "8",
+            "s",
+            "p",
+            "*",
+            "h",
+            "H",
+            "D",
+            "d",
+            "P",
+            "X",
+            "o",
+            "v",
+            "^",
+            "<",
+            ">",
+            "8",
+            "s",
+            "p",
+            "*",
+            "h",
+            "H",
+            "D",
+            "d",
+            "P",
+            "X",
+            "o",
+            "v",
+            "^",
+            "<",
+            ">",
+            "8",
+            "s",
+            "p",
+            "*",
+            "h",
+            "H",
+        )
+        c = sns.scatterplot(
+            x=f"Component {i+1}",
+            y=f"Component {i+2}",
+            hue="Antigens",
+            style="Antigens",
+            markers=markers,
+            data=df,
+            palette="Set3",
+            legend="brief" if j == 4 else False,
+            ax=ax[j + 2],
+        )
+        c.legend(loc="center left", bbox_to_anchor=(1, 0.5), ncol=1)
+        c.set_xlim(-xmax, xmax)
+        c.set_ylim(-ymax, ymax)
+
+        # Glycans
+        values1 = glyc[:, i]
+        values2 = glyc[:, i + 1]
+        data = {
+            f"Component {i+1}": values1,
+            f"Component {i+2}": values2,
+            "G": glycaninf["GS"],
+            "FB": glycaninf["FB"],
+        }
+        df = pd.DataFrame(data)
+        xmax = np.amax(np.absolute(values1))
+        ymax = np.amax(np.absolute(values2))
+        d = sns.scatterplot(
+            x=f"Component {i+1}",
+            y=f"Component {i+2}",
+            hue="G",
+            style="FB",
+            data=df,
+            palette="Paired",
+            legend="brief" if j == 4 else False,
+            ax=ax[j + 3],
+        )
+        d.legend(loc="center left", bbox_to_anchor=(1, 0.5), ncol=1)
+        d.set_xlim(-xmax, xmax)
+        d.set_ylim(-ymax, ymax)
+
+    ax[0].set_title("Subjects", fontsize=15)
+    ax[1].set_title("Receptors", fontsize=15)
+    ax[2].set_title("Antigens", fontsize=15)
+    ax[3].set_title("Glycans", fontsize=15)
+
+    return f
