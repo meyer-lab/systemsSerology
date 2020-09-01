64cf072beda5b58c644f252a2c0e339d862080ac
Aaron Meyer <ameyer@ucla.edu>
2020-09-01 07:50:45 -0700

    Update tests.

--- syserol/test/test_tensor.py
+++ syserol/test/test_tensor.py
@@ -1,31 +1,24 @@
 """
 Unit test file.
 """
-import unittest
 import numpy as np
 from ..dataImport import createCube
 from ..tensor import perform_CMTF
 
 
-class TestModel(unittest.TestCase):
-    """ Test Class for Tensor related work. """
-    def setUp(self):
-        self.cube, self.glyCube = createCube()
-
-    def test_R2X(self):
-        """ Test to ensure R2X for higher components is larger. """
-        arr = []
-        for i in range(1, 3):
-            facT, facM, tensorR2X, _ = perform_CMTF(self.cube, self.glyCube, i)
-            self.assertTrue(np.all(np.isfinite(facT.factors[0])))
-            self.assertTrue(np.all(np.isfinite(facT.factors[1])))
-            self.assertTrue(np.all(np.isfinite(facT.factors[2])))
-            self.assertTrue(np.allclose(facT.factors[0], facM.factors[0]))
-            arr.append(tensorR2X)
-
-        for j in range(len(arr) - 1):
-            self.assertTrue(arr[j] < arr[j + 1])
-
-        # confirm R2X is >= 0 and <=1
-        self.assertGreaterEqual(np.min(arr), 0)
-        self.assertLessEqual(np.max(arr), 1)
+def test_R2X():
+    """ Test to ensure R2X for higher components is larger. """
+    cube, glyCube = createCube()
+    arr = []
+    for i in range(1, 3):
+        facT, facM, tensorR2X, _ = perform_CMTF(cube, glyCube, i)
+        assert np.all(np.isfinite(facT.factors[0]))
+        assert np.all(np.isfinite(facT.factors[1]))
+        assert np.all(np.isfinite(facT.factors[2]))
+        assert np.allclose(facT.factors[0], facM.factors[0])
+        arr.append(tensorR2X)
+    for j in range(len(arr) - 1):
+        assert arr[j] < arr[j + 1]
+    # confirm R2X is >= 0 and <=1
+    assert np.min(arr) >= 0
+    assert np.max(arr) <= 1
