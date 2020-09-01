3815caaaf002e33f054c5bdff0467bab96578c1a
Aaron Meyer <ameyer@ucla.edu>
2020-09-01 07:44:34 -0700

    Reduce iter.

--- syserol/tensor.py
+++ syserol/tensor.py
@@ -47,7 +47,7 @@ def perform_CMTF(tensorIn, matrixIn, r):
     matrix[mask_matrix == 0] = 0.0
 
     # Initialize by running PARAFAC on the 3D tensor
-    parafacSettings = {'orthogonalise': 200, 'tol': 1e-11, 'normalize_factors': False, 'n_iter_max': 2000, 'linesearch': True}
+    parafacSettings = {'orthogonalise': 200, 'tol': 1e-9, 'normalize_factors': False, 'n_iter_max': 1000, 'linesearch': True}
     tensorFac = parafac(tensor, r, mask=mask, **parafacSettings)
 
     # Now run CMTF
