ed228a30dea21173bd0f1d022518a199c4fcee5c
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-09-02 12:52:01 -0700

    Adjustment (#77)
    * Adjustments
    
    * Cleaning
    
    * Adjust
    
    * Fix.
    
    * Reduce iter.
    
    * Update tests.

--- syserol/regression.py
+++ syserol/regression.py
@@ -10,7 +10,17 @@ from .dataImport import (
     functions,
     importAlterDF,
     getAxes,
+    functions,
 )
+
+def elasticNetFunc(X, Y):
+    """ Function with common elastic net methods. """
+    regr = ElasticNetCV(normalize=True, max_iter=10000, cv=len(Y))
+    regr.fit(X, Y)
+    enet = ElasticNet(alpha=regr.alpha_, normalize=True, max_iter=10000)
+    Y_pred = cross_val_predict(enet, X, Y, cv=len(Y))
+    rsq = np.sqrt(r2_score(Y, Y_pred))
+    return Y_pred, rsq
 
 
 def function_elastic_net(function="ADCC"):
@@ -20,24 +30,13 @@ def function_elastic_net(function="ADCC"):
 
     # separate dataframes
     df_func = df_merged[functions]
-    df_variables = df_merged.drop(
-        ["subject", "ADCD", "ADCC", "ADNP", "CD107a", "IFNy", "MIP1b"], axis=1
-    )
+    df_variables = df_merged.drop(["subject"] + functions, axis=1)
 
     # perform regression
     Y = df_func[function]
-    X = df_variables
-    Y_pred = np.empty(Y.shape)
+    Y_pred, rsq = elasticNetFunc(df_variables, Y)
 
-    regr = ElasticNetCV(normalize=True, max_iter=10000)
-    regr.fit(X, Y)
-    Y_pred = cross_val_predict(
-        ElasticNet(alpha=regr.alpha_, normalize=True, max_iter=10000), X, Y, cv=10
-    )
-
-    print(np.sqrt(r2_score(Y, Y_pred)))
-
-    return Y, Y_pred, np.sqrt(r2_score(Y, Y_pred))
+    return Y, Y_pred, rsq
 
 
 def noCMTF_function_prediction(tensorFac, function="ADCC"):
@@ -46,22 +45,13 @@ def noCMTF_function_prediction(tensorFac, function="ADCC"):
     df = pd.DataFrame(tensorFac[1][0])  # subjects x components matrix
     df = df.join(func, how="inner")
     df = df.dropna()
-    df_func = df[["ADCD", "ADCC", "ADNP", "CD107a", "IFNy", "MIP1b"]]
-    df_variables = df.drop(
-        ["subject", "ADCD", "ADCC", "ADNP", "CD107a", "IFNy", "MIP1b"], axis=1
-    )
+    df_func = df[functions]
+    df_variables = df.drop(["subject"] + functions, axis=1)
 
-    X = df_variables
     Y = df_func[function]
-    regr = ElasticNetCV(normalize=True, max_iter=10000)
-    regr.fit(X, Y)
-    Y_pred = cross_val_predict(
-        ElasticNet(alpha=regr.alpha_, normalize=True, max_iter=10000), X, Y, cv=10
-    )
+    Y_pred, rsq = elasticNetFunc(df_variables, Y)
 
-    print(f"Accuracy: {np.sqrt(r2_score(Y, Y_pred))}")
-
-    return Y, Y_pred, np.sqrt(r2_score(Y, Y_pred))
+    return Y, Y_pred, rsq
 
 def SVR_noCMTF_function_prediction(tensorFac, function="ADCC"):
     """ Predict functions using our decomposition and SVR regression methods"""
@@ -88,12 +78,10 @@ def ourSubjects_function_prediction(tensorFac, function="ADCC"):
     df_merged = importAlterDF()
 
     fullsubj = np.array(df_merged["subject"])  # Subjects only included in Alter
-    leftout = []
     subjects, _, _ = getAxes()
-    for index, i in enumerate(subjects):
-        if i not in fullsubj:
-            leftout.append((index, i))  # Subjects left out of Alter
-    indices = [i[0] for i in leftout]
+
+    # Subjects left out of Alter
+    indices = [idx for idx, i in enumerate(subjects) if i not in fullsubj]
 
     func, _ = importFunction()
     df = pd.DataFrame(tensorFac[1][0])  # subjects x components matrix
@@ -103,13 +91,8 @@ def ourSubjects_function_prediction(tensorFac, function="ADCC"):
     df_func = df[functions]
     df_variables = df.drop(["subject"] + functions, axis=1)
 
-    X = df_variables
     Y = df_func[function]
-    regr = ElasticNetCV(normalize=True, max_iter=10000)
-    regr.fit(X, Y)
-    Y_pred = cross_val_predict(
-        ElasticNet(alpha=regr.alpha_, normalize=True, max_iter=10000), X, Y, cv=10
-    )
+    Y_pred, _ = elasticNetFunc(df_variables, Y)
 
     return Y, Y_pred
 
