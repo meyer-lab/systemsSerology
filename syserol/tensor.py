b6bb720298b9fe30e3ce6140aa07176813180b3d
Aaron Meyer <ameyer@ucla.edu>
2020-09-03 08:20:19 -0700

    Speed.

--- syserol/tensor.py
+++ syserol/tensor.py
@@ -37,7 +37,7 @@ def cmtf(Y, mask_matrix, init):
     return KruskalTensor((None, [A, np.transpose(V)]))
 
 
-def perform_CMTF(tensorIn=None, matrixIn=None, r=8):
+def perform_CMTF(tensorIn=None, matrixIn=None, r=6):
     """ Perform CMTF decomposition. """
     if tensorIn is None:
         tensorIn, matrixIn = createCube()
