b830104055c08e91a58f97fc70f275e4b986270d
Aaron Meyer <ameyer@ucla.edu>
2020-09-03 07:25:28 -0700

    Fix Fig. 1

--- syserol/figures/figure1.py
+++ syserol/figures/figure1.py
@@ -12,7 +12,7 @@ def makeFigure():
     # Get list of axis objects
     ax, f = getSetup((3, 3), (1, 1))
 
-    comps = np.arange(1, 10)
+    comps = np.array([1, 2, 3, 4, 6, 8, 10, 12])
     tensorArr = np.empty(comps.size)
     matrixArr = np.empty(comps.size)
     for i, compp in enumerate(comps):
@@ -20,13 +20,13 @@ def makeFigure():
         tensorArr[i-1] = tensorR2X
         matrixArr[i-1] = matrixR2X
 
-    ax[0].plot(comps, tensorArr, label="Tensor R2X")
-    ax[0].plot(comps, matrixArr, label="Matrix R2X")
+    ax[0].scatter(comps, tensorArr, label="Tensor R2X")
+    ax[0].scatter(comps, matrixArr, label="Matrix R2X")
     ax[0].set_ylabel("R2X")
     ax[0].set_xlabel("Number of Components")
     ax[0].legend()
     ax[0].set_ylim(0, 1)
-    ax[0].set_xlim(1, np.amax(comps))
+    ax[0].set_xlim(0.5, np.amax(comps) + 0.5)
 
     # Add subplot labels
     subplotLabel(ax)
