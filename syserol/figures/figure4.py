f523c262d6a2290d7509803fac76d8c7200ede74
Madeleine Murphy <47797938+murphymadeleine21@users.noreply.github.com>
2020-09-04 14:00:13 -0700

    Make figure 3 without legends (#84)
    * Make figure 3 without legends
    
    * make common build figure

---
+++ syserol/figures/figure4.py
@@ -0,0 +1,9 @@
+"""
+This creates Figure 3 for the Paper, without the Legends.
+"""
+from .common import buildFigure3
+
+
+def makeFigure():
+    """ Generate Figure 3 for Paper, Showing Better Interpretation of All Data from Decomposed Tensor, without Legends"""
+    return buildFigure3(False)
