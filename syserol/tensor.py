b973d7924c66a028b718dfba2a53a7e3b99ec1a4
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-09-09 07:12:21 -0700

    Reorient and normalize (#91)
    * Add reorientation.
    
    * Add normalization
    
    * More normalization
    
    * Last normalization

--- syserol/tensor.py
+++ syserol/tensor.py
@@ -3,7 +3,7 @@ Tensor decomposition methods
 """
 import numpy as np
 import tensorly as tl
-from tensorly.kruskal_tensor import KruskalTensor
+from tensorly.kruskal_tensor import KruskalTensor, kruskal_normalise
 from tensorly.decomposition import parafac
 from .dataImport import createCube, load_cache
 
@@ -12,6 +12,18 @@ def calcR2X(data, factor):
     """ Calculate R2X. """
     tensorErr = np.nanvar(tl.kruskal_to_tensor(factor) - data)
     return 1.0 - tensorErr / np.nanvar(data)
+
+
+def reorient_factors(factors):
+    """ This function ensures that factors are negative on at most one direction. """
+    for jj in range(len(factors) - 1):
+        # Calculate the sign of the current factor in each component
+        means = np.sign(np.mean(factors[jj]**3, axis = 0))
+
+        # Update both the current and last factor
+        factors[jj] *= means[np.newaxis, :]
+        factors[-1] *= means[np.newaxis, :]
+    return factors
 
 
 def cmtf(Y, mask_matrix, init):
@@ -56,11 +68,14 @@ def perform_CMTF(tensorIn=None, matrixIn=None, r=6):
     matrix[mask_matrix == 0] = 0.0
 
     # Initialize by running PARAFAC on the 3D tensor
-    parafacSettings = {'orthogonalise': 500, 'tol': 1e-9, 'normalize_factors': False, 'n_iter_max': 2000}
+    parafacSettings = {'orthogonalise': True, 'tol': 1e-9, 'normalize_factors': False, 'n_iter_max': 2000}
     tensorFac = parafac(tensor, r, mask=mask, **parafacSettings)
+    tensorFac.factors = reorient_factors(tensorFac.factors)
+    tensorFac = kruskal_normalise(tensorFac)
 
     # Now run CMTF
     matrixFac = cmtf(matrix, mask_matrix=mask_matrix, init=tensorFac)
+    matrixFac = kruskal_normalise(matrixFac)
 
     # Solve for factors on remaining glycosylation matrix variation
     matrixResid = matrixIn - tl.kruskal_to_tensor(matrixFac)
@@ -68,6 +83,7 @@ def perform_CMTF(tensorIn=None, matrixIn=None, r=6):
 
     matrixFacExt = parafac(matrixResid, r, mask=mask_matrix, **parafacSettings)
     ncp = matrixFacExt.rank
+    matrixFacExt = kruskal_normalise(matrixFacExt)
 
     # Incorporate PCA into factorization
     tensorFac.factors[0] = np.concatenate((tensorFac.factors[0], matrixFacExt.factors[0]), axis=1)
@@ -77,7 +93,7 @@ def perform_CMTF(tensorIn=None, matrixIn=None, r=6):
     tensorFac.weights = np.pad(tensorFac.weights, (0, ncp), constant_values=1.0)
     matrixFac.factors[0] = tensorFac.factors[0]
     matrixFac.factors[1] = np.concatenate((matrixFac.factors[1], matrixFacExt.factors[1]), axis=1)
-    matrixFac.weights = np.pad(matrixFac.weights, (0, ncp), constant_values=1.0)
+    matrixFac.weights = np.concatenate((matrixFac.weights, matrixFacExt.weights))
     matrixFac.rank += ncp
 
     tensor_R2XX = calcR2X(tensorIn, tensorFac)
