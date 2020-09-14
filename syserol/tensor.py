53c04e98f56ecc5fccdba8308572515f367560a0
Aaron Meyer <ameyer@ucla.edu>
2020-09-14 10:00:22 -0700

    Add reverse back into tensor.

--- syserol/tensor.py
+++ syserol/tensor.py
@@ -1,6 +1,7 @@
 """
 Tensor decomposition methods
 """
+from copy import deepcopy
 import numpy as np
 import tensorly as tl
 from tensorly.kruskal_tensor import KruskalTensor, kruskal_normalise
@@ -68,7 +69,7 @@ def perform_CMTF(tensorIn=None, matrixIn=None, r=6):
     matrix[mask_matrix == 0] = 0.0
 
     # Initialize by running PARAFAC on the 3D tensor
-    parafacSettings = {'orthogonalise': True, 'tol': 1e-9, 'normalize_factors': False, 'n_iter_max': 1000}
+    parafacSettings = {'orthogonalise': True, 'tol': 1e-9, 'n_iter_max': 1000}
     tensorFac = parafac(tensor, r, mask=mask, **parafacSettings)
     tensorFac.factors = reorient_factors(tensorFac.factors)
     tensorFac = kruskal_normalise(tensorFac)
@@ -85,10 +86,16 @@ def perform_CMTF(tensorIn=None, matrixIn=None, r=6):
     ncp = matrixFacExt.rank
     matrixFacExt = kruskal_normalise(matrixFacExt)
 
+    # Go back to tensor
+    tensorResid = tensor - tl.kruskal_to_tensor(tensorFac)
+    tensorFacTwo = deepcopy(tensorFac)
+    tensorFacTwo.factors[0] = matrixFacExt.factors[0]
+    tensorFacExt = parafac(tensorResid, r, init=tensorFacTwo, fixed_modes=[0])
+
     # Incorporate PCA into factorization
-    tensorFac.factors[0] = np.concatenate((tensorFac.factors[0], matrixFacExt.factors[0]), axis=1)
-    tensorFac.factors[1] = np.pad(tensorFac.factors[1], ((0, 0), (0, ncp)), constant_values=0.0)
-    tensorFac.factors[2] = np.pad(tensorFac.factors[2], ((0, 0), (0, ncp)), constant_values=0.0)
+    for ii in range(3):
+        tensorFac.factors[ii] = np.concatenate((tensorFac.factors[ii], tensorFacExt.factors[ii]), axis=1)
+
     tensorFac.rank += ncp
     tensorFac.weights = np.pad(tensorFac.weights, (0, ncp), constant_values=1.0)
     matrixFac.factors[0] = tensorFac.factors[0]
