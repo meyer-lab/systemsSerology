edd4306891a2e323855136e1f2162d44ae907b12
Aaron Meyer <ameyer@ucla.edu>
2020-09-19 20:08:31 -0700

    Move to 4 components.

--- syserol/tensor.py
+++ syserol/tensor.py
@@ -50,7 +50,7 @@ def cmtf(Y, mask_matrix, init):
     return KruskalTensor((None, [A, np.transpose(V)]))
 
 
-def perform_CMTF(tensorIn=None, matrixIn=None, r=3):
+def perform_CMTF(tensorIn=None, matrixIn=None, r=4):
     """ Perform CMTF decomposition. """
     cacheMissing = load_cache()
 
