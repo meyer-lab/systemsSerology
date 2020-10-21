caf87db0c468bef886294e890f7519caa6faaaf2
Aaron Meyer <ameyer@ucla.edu>
2020-10-21 06:55:58 -0700

    Adjust factorization

--- syserol/tensor.py
+++ syserol/tensor.py
@@ -77,11 +77,11 @@ def perform_CMTF(tensorIn=None, matrixIn=None, r=6):
 
     jit_hvp = jit(hvp, static_argnums=(2, 3, 4, 5, 6))
 
-    facInit = parafac(tensorIn, r, mask=tmask, n_iter_max=100, orthogonalise=True)
+    facInit = parafac(tensorIn, r, mask=tmask, n_iter_max=200, orthogonalise=10)
     x0 = np.concatenate((np.ravel(facInit.factors[0]), np.ravel(facInit.factors[1]), np.ravel(facInit.factors[2])))
     x0 = np.concatenate((x0, randn(matrixIn.shape[1] * r)))
 
-    res = minimize(cost_jax, x0, method='trust-ncg', jac=cost_grad, hessp=jit_hvp, args=(tensorIn, matrixIn, tmask, mmask, r), options={"maxiter": 200})
+    res = minimize(cost_jax, x0, method='trust-ncg', jac=cost_grad, hessp=jit_hvp, args=(tensorIn, matrixIn, tmask, mmask, r), options={"maxiter": 1000})
     tensorFac, matrixFac = buildTensors(res.x, tensorIn, matrixIn, r)
     tensorFac = kruskal_normalise(tensorFac)
     matrixFac = kruskal_normalise(matrixFac)
