bc695eccc5d00ef054168a72918c7f8005d79e4c
Aaron Meyer <git@ameyer.me>
2020-10-21 20:48:19 -0700

    Remove skip.

--- syserol/test/test_import.py
+++ syserol/test/test_import.py
@@ -1,11 +1,9 @@
 """
 Unit test file.
 """
-import pytest
 from ..dataImport import load_file, importLuminex, createCube
 
 
-@pytest.mark.skip()
 def test_files():
     """ Test that files are successfully loaded. """
     load_file("data-luminex")
