c6215230721d7e2b71f575f3684dc79b5bfdfbad
Aaron Meyer <ameyer@ucla.edu>
2020-10-23 18:30:26 -0700

    Reduce fitting time

--- syserol/tensor.py
+++ syserol/tensor.py
@@ -81,11 +81,11 @@ def perform_CMTF(tensorOrig=None, matrixOrig=None, r=6):
 
     jit_hvp = jit(hvp, static_argnums=(2, 3, 4, 5))
 
-    CPinit = parafac(tensorIn.copy(), r, mask=tmask, n_iter_max=200, orthogonalise=10)
+    CPinit = parafac(tensorIn.copy(), r, mask=tmask, n_iter_max=400, orthogonalise=10)
     x0 = np.concatenate((np.ravel(CPinit.factors[0]), np.ravel(CPinit.factors[1]), np.ravel(CPinit.factors[2])))
 
     rgs = (tensorIn, matrixIn, tmask, r)
-    res = minimize(cost_jax, x0, method='trust-ncg', jac=cost_grad, hessp=jit_hvp, args=rgs, options={"disp": True, "maxiter": 800})
+    res = minimize(cost_jax, x0, method='trust-ncg', jac=cost_grad, hessp=jit_hvp, args=rgs, options={"disp": True, "maxiter": 400})
     tensorFac, matrixFac = buildTensors(res.x, tensorIn, matrixIn, tmask, r)
     tensorFac = kruskal_normalise(tensorFac)
     matrixFac = kruskal_normalise(matrixFac)
