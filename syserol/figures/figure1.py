321b3aeac171ff18e3aa31cf31735db987011255
Aaron Meyer <ameyer@ucla.edu>
2020-10-29 08:40:36 -0700

    Various adjustments.

--- syserol/figures/figure1.py
+++ syserol/figures/figure1.py
@@ -5,6 +5,7 @@ This creates Figure 1.
 import numpy as np
 from .common import subplotLabel, getSetup
 from ..regression import function_prediction
+from ..classify import class_predictions
 from ..tensor import perform_CMTF
 from ..dataImport import functions
 
@@ -16,23 +17,26 @@ def makeFigure():
 
     comps = np.arange(1, 11)
     tensorArr = np.zeros(comps.shape)
-    comp_accuracies = np.zeros(comps.shape)
+    pred_acc = np.zeros(comps.shape)
 
     for i in comps:
         tFac, _, R2X = perform_CMTF(r=i)
         tensorArr[i - 1] = R2X
         accur = [function_prediction(tFac, function=f, evaluation="all")[2] for f in functions]
-        comp_accuracies[i - 1] = np.mean(accur)
+        cp, nv = class_predictions(tFac[1][0])  # Our accuracies
+        accur.append(cp)
+        accur.append(nv)
+        pred_acc[i - 1] = np.mean(accur)
 
     ax[0].scatter(comps, tensorArr)
     ax[0].set_ylabel("R2X")
     ax[0].set_xlabel("Number of Components")
-    ax[0].legend()
     ax[0].set_ylim(0, 1)
     ax[0].set_xlim(0.5, np.amax(comps) + 0.5)
 
-    ax[1].scatter(comps, comp_accuracies)
-    ax[1].set_ylabel("Correlation Coefficient")
+    ax[1].scatter(comps, pred_acc)
+    ax[1].set_ylabel("Average prediction performance")
+    ax[1].set_xlabel("Number of Components")
     ax[1].set_ylim(0, 1)
 
     # Add subplot labels
