3264d54501aaeb5cf5cf770377117a445798ea6a
Aaron Meyer <ameyer@ucla.edu>
2020-11-01 15:26:50 -0800

    Fix

--- syserol/tensor.py
+++ syserol/tensor.py
@@ -3,13 +3,13 @@ Tensor decomposition methods
 """
 import numpy as np
 from scipy.optimize import minimize
+from scipy.linalg import khatri_rao
 import tensorly as tl
 from tensorly.decomposition import parafac
 from tensorly.cp_tensor import CPTensor, cp_normalize, unfolding_dot_khatri_rao
 from .dataImport import createCube
 
 tl.set_backend('numpy')
-config.update("jax_enable_x64", True)
 
 def calcR2X(tensorIn, matrixIn, tensorFac, matrixFac):
     """ Calculate R2X. """
@@ -44,15 +44,17 @@ def perform_CMTF(tensorOrig=None, matrixOrig=None, r=10):
         pinv *= np.dot(np.conj(tensorFac.factors[2].T), tensorFac.factors[2])
         pinv = np.conj(pinv.T)
 
-        kr_factors = tl.kr((tensorFac.factors[1], tensorFac.factors[2]))[~missing, :]
-        mttkrp = tl.dot(unfolded, kr_factors)
+        kr = khatri_rao(tensorFac.factors[1], tensorFac.factors[2])[~missing, :]
+        mttkrp = tl.dot(unfolded, kr)
         tensorFac.factors[0] = tl.solve(pinv, mttkrp.T).T
         matrixFac.factors[0] = tensorFac.factors[0]
 
         tensorFac = parafac(tensorIn, r, init=tensorFac, mask=tmask, fixed_modes=[0], n_iter_max=10)
 
         # Solve for the glycan matrix fit
-        matrixFac.factors[1] = np.linalg.lstsq(matrixFac.factors[0][selPat, :], matrixOrig[selPat, :])[0].T
+        matrixFac.factors[1] = np.linalg.lstsq(matrixFac.factors[0][selPat, :], matrixOrig[selPat, :], rcond=None)[0].T
+
+        print(calcR2X(tensorOrig, matrixIn, tensorFac, matrixFac))
 
     tensorFac = cp_normalize(tensorFac)
     matrixFac = cp_normalize(matrixFac)
