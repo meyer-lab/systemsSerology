6c46d3903bc32874c81d8d8251b6c53de5a2b7cc
Aaron Meyer <ameyer@ucla.edu>
2020-11-27 12:39:32 -0800

    Working

--- syserol/figures/figureS2.py
+++ syserol/figures/figureS2.py
@@ -4,8 +4,12 @@ This creates Figure S2.
 import numpy as np
 import seaborn as sns
 import tensorly as tl
+from scipy.optimize import least_squares
 from .common import subplotLabel, getSetup
 from ..dataImport import createCube, getAxes
+
+
+
 
 
 def makeFigure():
@@ -33,7 +37,16 @@ def makeFigure():
         miss = np.all(np.isfinite(data), axis=0)
         data = data[:, miss]
 
-        sns.regplot(x=data[0, :], y=data[1, :], ax=ax, lowess=True, scatter_kws={"s": 0.5})
+        ax.scatter(data[0, :], data[1, :], s=0.3)
+
+        func = lambda x: data[0, :] * x[0] / (data[0, :] * (1 - x[1]) + x[2]) - data[1, :]
+        diffscale = np.mean(data[1, :]) / np.mean(data[0, :])
+        popt = least_squares(func, x0 = [diffscale, 1.0, 1.0], jac="3-point")
+        xout = popt.x
+        linx = np.linspace(0.0, np.amax(data[0, :]), num=100)
+        liny = linx * xout[0] / (linx * (1 - xout[1]) + xout[2])
+        ax.plot(linx, liny, 'r-')
+
         ax.set_xlabel(detections[xi])
         ax.set_ylabel(detections[yi])
 
