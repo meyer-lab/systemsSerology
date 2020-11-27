70a25bb0d64e7816d2c1df4cacd07cc91df31434
Aaron Meyer <ameyer@ucla.edu>
2020-11-27 10:10:40 -0800

    Reorganize S2

--- syserol/dataImport.py
+++ syserol/dataImport.py
@@ -155,6 +155,12 @@ def createCube():
     # cube = cube / np.nanstd(cube, axis=(0, 2))[np.newaxis, :, np.newaxis]
     # glyCube = glyCube / np.nanstd(glyCube, axis=0)[np.newaxis, :]
 
+    # Clip to 0 as there are a few strongly negative outliers
+    # IIa.H/R were offset to negative, so correct that
+    cube[:, 2:4, :] += 17500
+    cube[:, 1:4, :] = np.clip(cube[:, 1:4, :], 0, None)
+    cube[:, 5:11, :] = np.clip(cube[:, 5:11, :], 0, None)
+
     print("Missingness fraction: " + str(np.mean(np.isnan(cube))))
 
     # Check that there are no slices with completely missing data
