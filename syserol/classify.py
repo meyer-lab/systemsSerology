67c2752c412714a727b75af3c20fe05265b61ae2
Aaron Meyer <ameyer@ucla.edu>
2020-12-03 17:17:46 -0800

    Simplified

--- syserol/classify.py
+++ syserol/classify.py
@@ -1,7 +1,7 @@
 """ Regression methods using Factorized Data. """
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import cross_val_predict, StratifiedKFold
+from sklearn.model_selection import cross_val_predict, cross_val_score, GridSearchCV
 from sklearn.linear_model import LogisticRegressionCV, LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
@@ -14,7 +14,10 @@ def getClassPred(X, df):
     Y1 = (df["class.cp"] == "controller").astype(int)  # controllers are 1s, progressors are 0s
     Y2 = (df["class.nv"] == "viremic").astype(int)  # viremic = 1, nonviremic = 0
 
-    return ClassifyHelper(X, Y1), ClassifyHelper(X, Y2), Y1, Y2
+    if X.shape[1] > 25:
+        return ClassifyHelper(X, Y1), ClassifyHelper(X, Y2), Y1, Y2
+    else:
+        return ClassifySVC(X, Y1), ClassifySVC(X, Y2), Y1, Y2
 
 
 def class_predictions(X, evaluation="all"):
@@ -42,7 +45,7 @@ def ClassifyHelper(X, Y):
     glmnet = LogitNet(alpha=.8, n_jobs=10, n_splits=10).fit(X, Y)
     score = glmnet.cv_mean_score_[glmnet.lambda_best_ == glmnet.lambda_path_][0]
 
-    Y_pred = cross_val_predict(glmnet, X, Y, cv=StratifiedKFold(n_splits=10), n_jobs=-1)
+    Y_pred = cross_val_predict(glmnet, X, Y, cv=10, n_jobs=-1)
 
     # TODO: Note that the accuracy on cross-validation is slightly lower than what glmnet returns.
     # score vs. accuracy_score(Y, Y_pred)
@@ -50,32 +53,12 @@ def ClassifyHelper(X, Y):
 
 
 def ClassifySVC(X, Y):
-    if X.shape[1] < 2:
-        return np.zeros(Y.shape[0]), 0, np.zeros(X.shape[1])
-    Cs = np.logspace(-4, 4, num=50)
-    kernels = ['linear', 'rbf']
-    skf = StratifiedKFold(n_splits=10)
-    comp = X.shape[1]
-    values_all = []
-    for C in Cs:
-        for kernel in kernels:
-            for i in range(0, comp - 1):
-                for j in range(i + 1, comp):
-                    folds = []
-                    Y_preds = []
-                    for train, test in skf.split(X, Y):
-                        double = np.vstack((X[train, i], X[train, j])).T
-                        clf = SVC(kernel=kernel, C=C).fit(double, Y[train])
-                        test_double = np.vstack((X[test, i], X[test, j])).T
-                        Y_pred = clf.predict(test_double)
-                        Y_preds.append(Y_pred)
-                        score = accuracy_score(Y[test], Y_pred)
-                        folds.append([score])
-                    values_all.append([i, j, np.mean(folds), folds, Y_preds, kernel, C])
-    df_comp = pd.DataFrame(values_all)
-    df_comp.columns = ["First", "Second", "Score", "All_scores", "Y_pred", "kernel", "C"]
-    df_comp = df_comp.sort_values(by="Score", ascending=False)
-    components = np.zeros(comp)
-    components[df_comp.iloc[0, 0]] = 1
-    components[df_comp.iloc[0, 1]] = 1
-    return df_comp.iloc[0, 4], df_comp.iloc[0, 2], components
+    param_grid = {"C": np.logspace(-3, 3, num=20), "kernel": ["rbf", "linear"]}
+
+    grid = GridSearchCV(SVC(), param_grid, n_jobs=-1, cv=20)
+    grid.fit(X, Y)
+    score = grid.best_score_
+    print(grid.best_estimator_)
+    Y_pred = cross_val_predict(grid.best_estimator_, X, Y, cv=20, n_jobs=-1)
+
+    return Y_pred, score, np.zeros(X.shape[1])
