ba5618ad633b949b6f8022c7ceb8b8d30f4fdb89
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-12-30 15:35:57 -0800

    Other edits (#208)
    * Make changes
    
    * Fix
    
    * Try this.
    
    * Fix

--- syserol/regression.py
+++ syserol/regression.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import scale
 from sklearn.linear_model import ElasticNetCV, LogisticRegressionCV
 from sklearn.gaussian_process import GaussianProcessClassifier, GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import ConstantKernel, RBF, WhiteKernel
+from sklearn.model_selection import KFold, StratifiedKFold
 from scipy.stats import pearsonr
 from .dataImport import (
     importFunction,
@@ -48,15 +49,19 @@ def function_prediction(Xin, function="ADCC", evaluation="all"):
 
 def RegressionHelper(X, Y, classify=False):
     """ Function with the regression cross-validation strategy. """
-    kern = ConstantKernel() * RBF(length_scale=np.ones(X.shape[1])) + WhiteKernel()
+    kern = RBF(length_scale=np.ones(X.shape[1]), length_scale_bounds=(1e-5, 1e9))
+    kern = ConstantKernel() * kern
+    kern = kern + WhiteKernel(noise_level_bounds=(1e-3, 1e2))
 
     if classify:
         X = scale(X)
         est = LogisticRegressionCV(penalty="elasticnet", solver="saga")
         estG = GaussianProcessClassifier(kern, warm_start=True, n_restarts_optimizer=5)
+        cv = StratifiedKFold(n_splits=20, shuffle=True)
     else:
         est = ElasticNetCV(normalize=True)
         estG = GaussianProcessRegressor(kern, normalize_y=True, n_restarts_optimizer=5)
+        cv = KFold(n_splits=20, shuffle=True)
 
     est.l1_ratios = [0.8]
     est.cv = 10
@@ -65,10 +70,11 @@ def RegressionHelper(X, Y, classify=False):
     est = est.fit(X, Y)
     coef = np.squeeze(est.coef_)
 
-    Y_pred = cross_val_predict(est, X, Y, cv=20, n_jobs=-1)
+    Y_pred = cross_val_predict(est, X, Y, cv=cv, n_jobs=-1)
 
     if X.shape[1] < 20:
-        Y_pred_G = cross_val_predict(estG, X, Y, cv=20, n_jobs=-1)
+        estG.fit(X, Y)
+        Y_pred_G = cross_val_predict(estG, X, Y, cv=cv, n_jobs=-1)
 
         if classify:
             better = accuracy_score(Y, Y_pred_G) > accuracy_score(Y, Y_pred)
