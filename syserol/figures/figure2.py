6524213c64667bdc586f36b98863a5456b3906e4
Hakan Alpay <hak7alp@gmail.com>
2021-04-06 23:00:06 -0700

    fixed my editor's autoformatting

--- syserol/figures/figure2.py
+++ syserol/figures/figure2.py
@@ -1,36 +1,28 @@
 """
 This creates Figure 2.
 """
-
 import numpy as np
 from statsmodels.multivariate.pca import PCA
 from .common import subplotLabel, getSetup
 from ..tensor import perform_CMTF
 from ..dataImport import functions, createCube
 from matplotlib.ticker import ScalarFormatter
-
-
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
     ax, f = getSetup((6, 3), (1, 2))
-
     comps = np.arange(1, 12)
     TMTFR2X = np.zeros(comps.shape)
     PCAR2X = np.zeros(comps.shape)
-
     tOrig, mOrig = createCube()
-
     tMat = np.reshape(tOrig, (181, -1))
     tMat = tMat[:, ~np.all(np.isnan(tMat), axis=0)]
     tMat = np.hstack((tMat, mOrig))
-
     sizePCA = comps * np.sum(tMat.shape)
     sizeTfac = comps * (np.sum(tOrig.shape) + mOrig.shape[1])
 
     for i, cc in enumerate(comps):
-        outt = PCA(tMat, ncomp=cc, missing="fill-em",
-                   standardize=False, demean=False, normalize=False)
+        outt = PCA(tMat, ncomp=cc, missing="fill-em", standardize=False, demean=False, normalize=False)
         recon = outt.scores @ outt.loadings.T
         PCAR2X[i] = np.nanvar(tMat - recon) / np.nanvar(tMat)
 
@@ -40,8 +32,7 @@ def makeFigure():
     ax[0].set_ylabel("TMTF R2X")
     ax[0].set_xlabel("Number of Components")
     ax[0].set_xticks([x for x in comps])
-    ax[0].set_xticklabels([x for x in comps])
-    ax[0].set_ylim(0, 1)
+	@@ -44,8 +45,8 @@ def makeFigure():
     ax[0].set_xlim(0.0, np.amax(comps) + 0.5)
 
     ax[1].set_xscale("log", base=2)
@@ -53,8 +44,6 @@ def makeFigure():
     ax[1].set_xlim(2 ** 8, 2 ** 12)
     ax[1].xaxis.set_major_formatter(ScalarFormatter())
     ax[1].legend()
-
     # Add subplot labels
     subplotLabel(ax)
-
     return f
