d389a21fb09c5a25f12359498738651ddf60e882
Aaron Meyer <ameyer@ucla.edu>
2021-04-11 15:18:39 -0700

    Some adjustments

--- syserol/impute.py
+++ syserol/impute.py
@@ -6,17 +6,21 @@ from .dataImport import createCube
 from .tensor import perform_CMTF
 
 
-def evaluate_missing(comps, numSample=15):
-    """ check differences between original and recon values for different number of components. """
+def evaluate_missing(comps, numSample, chords=True):
+    """ check differences between original and recon values for different number of components.
+    chords: whether to leave out tensor chords or individual values """
     cube, glyCube = createCube()
-    np.random.seed(1)  # Avoid random variation in this output
 
     R2X = np.zeros(comps.shape)
     missingCube = np.copy(cube)
     for _ in range(numSample):
         idxs = np.argwhere(np.isfinite(missingCube))
-        _, j, k = idxs[np.random.choice(idxs.shape[0], 1)][0]
-        missingCube[:, j, k] = np.nan
+        i, j, k = idxs[np.random.choice(idxs.shape[0], 1)][0]
+
+        if chords:
+            missingCube[:, j, k] = np.nan
+        else:
+            missingCube[i, j, k] = np.nan
 
     imputeVals = np.copy(cube)
     imputeVals[np.isfinite(missingCube)] = np.nan
