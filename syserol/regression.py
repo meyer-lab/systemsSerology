c0057f14151ba84665ae997288e90734a5750077
Aaron Meyer <ameyer@ucla.edu>
2021-04-11 07:59:03 -0700

    Try this

--- syserol/regression.py
+++ syserol/regression.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import accuracy_score
+from sklearn.utils import resample
 from sklearn.preprocessing import scale
 from sklearn.linear_model import ElasticNetCV, LogisticRegressionCV, LogisticRegression, ElasticNet
 from sklearn.gaussian_process import GaussianProcessClassifier, GaussianProcessRegressor
@@ -17,7 +18,7 @@ from .dataImport import (
 )
 
 
-def function_elastic_net(function="ADCC", random=False):
+def function_elastic_net(function="ADCC", **kwargs):
     """ Predict functions using elastic net according to Alter methods"""
     # Import Luminex, Luminex-IGG, Function, and Glycan into DF
     df = importAlterDF(function=True, subjects=False).dropna()
@@ -25,15 +26,13 @@ def function_elastic_net(function="ADCC", random=False):
     # separate dataframes
     Y = df[function]
     X = df.drop(["subject"] + functions, axis=1)
-    if random == True:
-        X = X.sample(frac=1)
 
     # perform regression
     Y_pred, coef = RegressionHelper(X, Y)
     return Y, Y_pred, pearsonr(Y, Y_pred)[0], coef
 
 
-def function_prediction(Xin, function="ADCC"):
+def function_prediction(Xin, function="ADCC", **kwargs):
     """ Predict functions using our decomposition and regression methods"""
     func, _ = importFunction()
 
@@ -41,20 +40,44 @@ def function_prediction(Xin, function="ADCC"):
     subset = np.isfinite(Y)
     X = Xin[subset, :]  # subjects x components matrix
     Y = Y[subset]
+    assert Y.dtype == float
 
     # Perform Regression
-    Y_pred, coef = RegressionHelper(X, Y)
+    Y_pred, coef = RegressionHelper(X, Y, **kwargs)
     accuracy = pearsonr(*selectAlter(Y, Y_pred, subset))[0]
 
     return Y, Y_pred, accuracy, coef
 
 
-def RegressionHelper(X, Y, classify=False):
+def make_regression_df(X):
+    """ Make the dataframe of prediction accuracies. """
+    # Gather Function Prediction Accuracies
+    preds = [function_prediction(X, function=f)[2] for f in functions]
+
+    accuracies = [function_elastic_net(f)[2] for f in functions]
+    baselines = [function_prediction(X, function=f, randomize=True)[2] for f in functions]
+    accuracies = accuracies + preds + baselines
+
+    # Create DataFrame
+    model = ["TMTF"] * 6 + ["Alter et al"] * 6 + ["Randomized"] * 6
+    function = functions + functions + functions
+    data = {"Accuracy": accuracies, "Model": model, "Function": function}
+    functions_df = pd.DataFrame(data)  # Function Prediction DataFrame, Figure 5A
+    return functions_df
+
+
+def RegressionHelper(X, Y, randomize=False, resample=False):
     """ Function with the regression cross-validation strategy. """
     kern = ConstantKernel() * RBF(np.ones(X.shape[1]), (1e-2, 1e14))
     kern += WhiteKernel(noise_level_bounds=(0.001, 0.8))
 
-    if classify:
+    if randomize:
+        X = np.random.shuffle(X)
+
+    if resample:
+        X, Y = resample(X, Y)
+
+    if Y.dtype == int:
         X = scale(X)
         estCV = LogisticRegressionCV(penalty="elasticnet", solver="saga", cv=10, l1_ratios=[0.8], n_jobs=-1, max_iter=1000000)
         estCV.fit(X, Y)
@@ -62,6 +85,7 @@ def RegressionHelper(X, Y, classify=False):
         estG = GaussianProcessClassifier(kern, n_restarts_optimizer=40)
         cv = StratifiedKFold(n_splits=10, shuffle=True)
     else:
+        assert Y.dtype == float
         estCV = ElasticNetCV(normalize=True, l1_ratio=0.8, cv=10, n_jobs=-1, max_iter=1000000)
         estCV.fit(X, Y)
         est = ElasticNet(normalize=True, alpha=estCV.alpha_, l1_ratio=0.8, max_iter=1000000)
@@ -79,7 +103,7 @@ def RegressionHelper(X, Y, classify=False):
         estG.kernel = estG.kernel_
         Y_pred_G = cross_val_predict(estG, X, Y, cv=cv, n_jobs=-1)
 
-        if classify:
+        if Y.dtype == int:
             better = accuracy_score(Y, Y_pred_G) > accuracy_score(Y, Y_pred)
         else:
             better = pearsonr(Y, Y_pred_G)[0] > pearsonr(Y, Y_pred)[0]
@@ -87,4 +111,4 @@ def RegressionHelper(X, Y, classify=False):
         if better:
             return Y_pred_G, coef
 
-    return Y_pred, coef
+    return Y_pred, coef, X, Y
