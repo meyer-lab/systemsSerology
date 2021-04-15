e11af9694d02770790080e1848a2cf9831c25a93
ctan <cyztan@gmail.com>
2021-04-15 01:01:15 -0700

    Fix Fig. 3b

--- syserol/figures/figure3.py
+++ syserol/figures/figure3.py
@@ -4,13 +4,13 @@ This creates Figure 3.
 
 import numpy as np
 from .common import subplotLabel, getSetup
-from ..impute import evaluate_missing
+from ..impute import evaluate_missing, evaluate_missing_meas
 
 
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
-    ax, f = getSetup((6, 2), (1, 3))
+    ax, f = getSetup((9, 3), (1, 3))
 
     comps = np.arange(1, 12)
 
@@ -22,13 +22,15 @@ def makeFigure():
     ax[0].set_xticklabels([x for x in comps])
     ax[0].set_ylim(0, 1)
 
-    Q2X = evaluate_missing(comps, indv=True)
-    ax[1].scatter(comps, Q2X)
+    CMTFR2X, PCAR2X = evaluate_missing_meas(comps)
+    ax[1].plot(comps, CMTFR2X, ".", label="CMTF")
+    ax[1].plot(comps, 1.0 - PCAR2X, ".", label="PCA")
     ax[1].set_ylabel("Q2X of Imputation")
     ax[1].set_xlabel("Number of Components")
     ax[1].set_xticks([x for x in comps])
     ax[1].set_xticklabels([x for x in comps])
     ax[1].set_ylim(0, 1)
+    ax[1].legend()
 
     ax[2].scatter([], [])
     ax[2].set_ylabel("Q2X of Imputation")
--- syserol/impute.py
+++ syserol/impute.py
@@ -2,11 +2,20 @@
 
 import numpy as np
 import tensorly as tl
+from statsmodels.multivariate.pca import PCA
 from .dataImport import createCube
 from .tensor import perform_CMTF
 
 
-def evaluate_missing(comps, numSample=15, indv=False):
+def flatten_to_mat(tensor, matrix):
+    n = tensor.shape[0]
+    tMat = np.reshape(tensor, (n, -1))
+    tMat = tMat[:, ~np.all(np.isnan(tMat), axis=0)]
+    tMat = np.hstack((tMat, matrix))
+    return tMat
+
+
+def evaluate_missing(comps, numSample=15):
     """ check differences between original and recon values for different number of components. """
     cube, glyCube = createCube()
     np.random.seed(1)  # Avoid random variation in this output
@@ -15,11 +24,8 @@ def evaluate_missing(comps, numSample=15, indv=False):
     missingCube = np.copy(cube)
     for _ in range(numSample):
         idxs = np.argwhere(np.isfinite(missingCube))
-        i, j, k = idxs[np.random.choice(idxs.shape[0], 1)][0]
-        if indv == True:
-            missingCube[i, j, k] = np.nan
-        else:
-            missingCube[:, j, k] = np.nan
+        _, j, k = idxs[np.random.choice(idxs.shape[0], 1)][0]
+        missingCube[:, j, k] = np.nan
 
     imputeVals = np.copy(cube)
     imputeVals[np.isfinite(missingCube)] = np.nan
@@ -33,3 +39,37 @@ def evaluate_missing(comps, numSample=15, indv=False):
         R2X[ii] = 1.0 - np.nanvar(tensorR - imputeVals) / np.nanvar(imputeVals)
 
     return R2X
+
+def evaluate_missing_meas(comps, numSample=15):
+    """ check differences between original and recon values for different number of components. """
+    cube, glyCube = createCube()
+    np.random.seed(1)  # Avoid random variation in this output
+
+    PCAR2X = np.zeros(comps.shape)
+    CMTFR2X = np.zeros(comps.shape)
+    missingCube = np.copy(cube)
+    for _ in range(numSample):
+        idxs = np.argwhere(np.isfinite(missingCube))
+        i, j, k = idxs[np.random.choice(idxs.shape[0], 1)][0]
+        missingCube[i, j, k] = np.nan
+
+    imputeVals = np.copy(cube)
+    imputeVals[np.isfinite(missingCube)] = np.nan
+
+    missingMat = flatten_to_mat(missingCube, glyCube)
+    imputeMat = np.copy(flatten_to_mat(cube, glyCube))
+    imputeMat[np.isfinite(missingMat)] = np.nan
+
+    for ii, nComp in enumerate(comps):
+        # reconstruct with some values missing
+        tensorR = tl.cp_to_tensor(perform_CMTF(missingCube, glyCube, nComp))
+        tensorR[np.isfinite(missingCube)] = np.nan
+
+        # Compare original Cube with reconstructed cube, which was created from the cube with imputed missing values
+        CMTFR2X[ii] = 1.0 - np.nanvar(tensorR - imputeVals) / np.nanvar(imputeVals)
+
+        outt = PCA(missingMat, ncomp=nComp, missing="fill-em", standardize=False, demean=False, normalize=False)
+        recon = outt.scores @ outt.loadings.T
+        PCAR2X[ii] = 1.0 - np.nanvar(recon - imputeMat) / np.nanvar(imputeMat)
+
+    return CMTFR2X, PCAR2X
