121cf4da7a121dbdb0ba54004f82a7ae92b0180b
Aaron Meyer <ameyer@ucla.edu>
2021-05-03 08:26:53 -0700

    Revert

--- syserol/regression.py
+++ syserol/regression.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.model_selection import cross_val_predict
 from sklearn.utils import resample as resampleSK
 from sklearn.preprocessing import scale
-from sklearn.linear_model import ElasticNetCV, LogisticRegressionCV, LogisticRegression, ElasticNet, LinearRegression
+from sklearn.linear_model import ElasticNetCV, LogisticRegressionCV, LogisticRegression, ElasticNet
 from sklearn.model_selection import KFold
 from scipy.stats import pearsonr
 from .dataImport import (
@@ -79,21 +79,14 @@ def RegressionHelper(X, Y, randomize=False, resample=False):
     cv = KFold(n_splits=10, shuffle=True)
 
     if Y.dtype == int:
-        if X.shape[1] > 20:
-            estCV = LogisticRegressionCV(Cs=20, penalty="elasticnet", solver="saga", cv=cv, l1_ratios=[0.8], n_jobs=-1, max_iter=1000000)
-            estCV.fit(X, Y)
-            est = LogisticRegression(C=estCV.C_[0], penalty="elasticnet", solver="saga", l1_ratio=0.8, max_iter=1000000)
-        else:
-            est = LogisticRegression(penalty="none")
+        estCV = LogisticRegressionCV(penalty="elasticnet", solver="saga", cv=cv, l1_ratios=[0.8], n_jobs=-1, max_iter=1000000)
+        estCV.fit(X, Y)
+        est = LogisticRegression(C=estCV.C_[0], penalty="elasticnet", solver="saga", l1_ratio=0.8, max_iter=1000000)
     else:
         assert Y.dtype == float
-
-        if X.shape[1] > 20:
-            estCV = ElasticNetCV(normalize=True, l1_ratio=0.8, cv=cv, n_jobs=-1, max_iter=1000000)
-            estCV.fit(X, Y)
-            est = ElasticNet(normalize=True, alpha=estCV.alpha_, l1_ratio=0.8, max_iter=1000000)
-        else:
-            est = LinearRegression(normalize=True)
+        estCV = ElasticNetCV(normalize=True, l1_ratio=0.8, cv=cv, n_jobs=-1, max_iter=1000000)
+        estCV.fit(X, Y)
+        est = ElasticNet(normalize=True, alpha=estCV.alpha_, l1_ratio=0.8, max_iter=1000000)
 
     est = est.fit(X, Y)
     coef = np.squeeze(est.coef_)
