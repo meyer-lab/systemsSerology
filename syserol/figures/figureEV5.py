2b26d569ddcd65e24e77fc2fc05e04478484058a
Madeleine <murphym21@g.ucla.edu>
2021-05-04 17:23:08 -0700

    try geometric mean

--- syserol/figures/figureEV5.py
+++ syserol/figures/figureEV5.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import pandas as pd 
 import seaborn as sns
+from scipy.stats import gmean
 from .common import getSetup, subplotLabel
 from ..dataImport import importAlterDF
 
@@ -16,13 +17,13 @@ def makeFigure():
 
     test = pd.DataFrame(columns=["Class", "IgG", "Value"])
     for ig in IgGs:
-        gp_id = gp120.loc[:, gp120.columns.str.contains(ig)].mean(axis=1)
-        p24_id = p24.loc[:, p24.columns.str.contains(ig)].mean(axis=1)
+        gp_id = gp120.loc[:, gp120.columns.str.contains(ig)].apply(gmean, 1)  # apply(np.geomean)
+        p24_id = p24.loc[:, p24.columns.str.contains(ig)].apply(gmean, 1)
         data = {"Class":df["class.etuv"], "IgG":[ig]*181, "Value": gp_id/p24_id}
         test = pd.concat([test, pd.DataFrame(data)])
     
-    # Clip values < 0 or > 100
-    test["Value"] = test["Value"].clip(0,100)
+    # Clip values < 0
+    test["Value"] = test["Value"].clip(0)
 
     # Plot
     sns.boxplot(x="IgG", y="Value", hue="Class", data=test, palette="colorblind", ax=ax[0])
