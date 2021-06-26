7380d81f6f1f12a00dd95587917b4116b6f3961b
Cyrillus Tan <cyztan@gmail.com>
2021-06-26 16:14:19 -0700

    Make COVID sample time plot (#342)
    * Make COVID sample time plot
    
    * Edits
    
    * Fix tests
    
    Co-authored-by: Aaron Meyer <ameyer@ucla.edu>

--- syserol/COVID.py
+++ syserol/COVID.py
@@ -20,7 +20,7 @@ def pbsSubtractOriginal():
 
 
 def to_slice(subjects, df):
-    _, Rlabels, AgLabels = dimensionLabel4D()
+    Rlabels, AgLabels = dimensionLabel3D()
     tensor = np.full((len(subjects), len(AgLabels), len(Rlabels)), np.nan)
     missing = 0
 
@@ -59,7 +59,7 @@ def Tensor3D():
     tensor = np.log10(tensor)
 
     # Mean center each measurement
-    tensor -= np.nanmean(tensor)
+    tensor -= np.nanmean(tensor, axis=0)
 
     return tensor, np.array(df.index)
 
@@ -79,7 +79,7 @@ def dimensionLabel3D():
         "FcR3A",
         "FcR3B"
     ]
-    antigenLabel = ["S", "RBD", "N", "S1", "S2", "S1 Trimer", "flu_mix", "NL63", "HKU1"]
+    antigenLabel = ["S", "RBD", "N", "S1", "S2", "S1 Trimer"]
     return receptorLabel, antigenLabel
 
 
@@ -92,3 +92,16 @@ def COVIDpredict(item):
     y = df[~df.index.duplicated(keep='first')][item].loc[subjects]
     Y_pred, coef, XX, YY = RegressionHelper(X, pd.factorize(y)[0])
     return np.sum(Y_pred == YY)/len(y)
+
+
+def time_components_df(tfac, condition=None):
+    subj = pbsSubtractOriginal()
+    df = pd.DataFrame(tfac.factors[0])
+    df.columns = ["Comp. " + str((i + 1)) for i in range(tfac.factors[0].shape[1])]
+    df['days'] = subj['days'].values
+    if condition is not None:
+        df = df.loc[(subj["group"] == condition).values, :]
+    df = df.dropna()
+    df = pd.melt(df, ['days'])
+    df.columns = ["Days", "Factors", "Value"]
+    return df
--- syserol/test/test_import.py
+++ syserol/test/test_import.py
@@ -3,7 +3,7 @@ Unit test file.
 """
 import numpy as np
 from ..dataImport import load_file, importLuminex, createCube
-from ..COVID import pbsSubtractOriginal, Tensor4D
+from ..COVID import pbsSubtractOriginal, Tensor3D
 
 
 def test_files():
@@ -16,10 +16,6 @@ def test_files():
 def test_COVID_import():
     """ Test COVID import functions. """
     pbsSubtractOriginal()
-    tensor, subjects = Tensor4D()
-    assert tensor.shape[0] == len(subjects)
+    tensor, _ = Tensor3D()
 
-    # assert np.all(np.mean(np.isfinite(tensor), axis=(1, 2, 3))) > 0.1)
-    assert np.all(np.mean(np.isfinite(tensor), axis=(0, 2, 3)) > 0.1)
-    assert np.all(np.mean(np.isfinite(tensor), axis=(0, 1, 3)) > 0.1)
-    assert np.all(np.mean(np.isfinite(tensor), axis=(0, 1, 2)) > 0.1)
+    assert np.all(np.isfinite(tensor))
