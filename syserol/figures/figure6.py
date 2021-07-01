0914dde1cba4d89819052f21c9848ab123123524
Cyrillus Tan <cyztan@gmail.com>
2021-07-01 13:55:28 -0700

    Separate time plots for COVID patient outcomes (#349)
    * Separate time plots for COVID patient outcomes
    
    * Smaller scatter points
    
    * Week-binned component boxplot
    
    * Adjustments
    
    * Column name change
    
    Co-authored-by: Aaron Meyer <ameyer@ucla.edu>

--- syserol/figures/figure6.py
+++ syserol/figures/figure6.py
@@ -1,8 +1,9 @@
 """ This makes Figure 6. Plot of R2X values"""
 import numpy as np
 import seaborn as sns
+from tensorly.decomposition import parafac
 from ..COVID import Tensor3D, dimensionLabel3D, time_components_df
-from ..tensor import perform_CMTF, calcR2X, tensor_degFreedom
+from ..tensor import calcR2X, tensor_degFreedom, cp_normalize, reorient_factors, sort_factors
 from .common import getSetup, subplotLabel
 from ..impute import flatten_to_mat
 from statsmodels.multivariate.pca import PCA
@@ -10,12 +11,21 @@ from matplotlib.ticker import ScalarFormatter
 
 
 def makeFigure():
-    ax, f = getSetup((14, 7), (2, 4))
-    comps = np.arange(1, 9)
+    ax, f = getSetup((14, 10), (3, 4))
+    comps = np.arange(1, 7)
 
     tensor, _ = Tensor3D()
-    CMTFfacs = [perform_CMTF(tensor, r=cc) for cc in comps]
-    CMTFR2X = np.array([f.R2X for f in CMTFfacs])
+
+    CMTFfacs = [parafac(tensor, cc, tol=1e-12, n_iter_max=4000, linesearch=True, orthogonalise=2) for cc in comps]
+
+    # Normalize factors
+    CMTFfacs = [cp_normalize(f) for f in CMTFfacs]
+    CMTFfacs = [reorient_factors(f) for f in CMTFfacs]
+    CMTFfacs = [sort_factors(f) if i > 0 else f for i, f in enumerate(CMTFfacs)]
+
+    # Calculate R2X
+    CMTFR2X = np.array([calcR2X(f, tensor) for f in CMTFfacs])
+    print(CMTFR2X)
 
     ax[0].axis("off")
     ax[1].scatter(comps, CMTFR2X, color="b")
@@ -51,18 +61,24 @@ def makeFigure():
     # Colormap
 
     Rlabels, agLabels = dimensionLabel3D()
-    tfac = CMTFfacs[3]
+    tfac = CMTFfacs[2]
+
+    # Flip comp. 2
+    tfac.factors[0][:, 1] *= -1
+    tfac.factors[2][:, 1] *= -1
 
     components = [str(ii + 1) for ii in range(tfac.rank)]
-    comp_plot(tfac.factors[0], components, False, "Samples", ax[4])
-    comp_plot(tfac.factors[1], components, agLabels, "Antigens", ax[5])
-    comp_plot(tfac.factors[2], components, Rlabels, "Receptors", ax[6])
+    comp_plot(tfac.factors[0], components, False, "Samples", ax[3])
+    comp_plot(tfac.factors[1], components, agLabels, "Antigens", ax[4])
+    comp_plot(tfac.factors[2], components, Rlabels, "Receptors", ax[5])
 
-    df = time_components_df(tfac)
-    sns.regplot(data=df.loc[df["Factors"] == "Comp. 1", :], x="Days", y="Value", ax=ax[7], lowess=True, color="r", marker='.')
-    sns.regplot(data=df.loc[df["Factors"] == "Comp. 2", :], x="Days", y="Value", ax=ax[7], lowess=True, color="g", marker='.')
-    sns.regplot(data=df.loc[df["Factors"] == "Comp. 3", :], x="Days", y="Value", ax=ax[7], lowess=True, color="b", marker='.')
-    sns.regplot(data=df.loc[df["Factors"] == "Comp. 4", :], x="Days", y="Value", ax=ax[7], lowess=True, color="k", marker='.')
+    time_plot(tfac, ax[7])
+    time_plot(tfac, ax[8], condition="Negative")
+    time_plot(tfac, ax[9], condition="Moderate")
+    time_plot(tfac, ax[10], condition="Severe")
+    time_plot(tfac, ax[11], condition="Deceased")
+
+    #sns.boxplot(data=df.loc[df["week"] == 3, :], x="variable", y="value", hue="group")
 
     subplotLabel(ax)
     return f
@@ -73,3 +89,15 @@ def comp_plot(factors, xlabel, ylabel, plotLabel, ax):
     sns.heatmap(factors, cmap="PiYG", center=0, xticklabels=xlabel, yticklabels=ylabel, ax=ax)
     ax.set_xlabel("Components")
     ax.set_ylabel(plotLabel)
+
+
+def time_plot(tfac, ax, condition=None):
+    df = time_components_df(tfac, condition=condition)
+    sns.regplot(data=df.loc[df["Factors"] == "Comp. 1", :], x="days", y="value", ax=ax, lowess=True, color="r",
+                marker='.', scatter_kws={"s": 10})
+    sns.regplot(data=df.loc[df["Factors"] == "Comp. 2", :], x="days", y="value", ax=ax, lowess=True, color="g",
+                marker='.', scatter_kws={"s": 10})
+    sns.regplot(data=df.loc[df["Factors"] == "Comp. 3", :], x="days", y="value", ax=ax, lowess=True, color="b",
+                marker='.', scatter_kws={"s": 10})
+    if condition is not None:
+        ax.set_title(condition + " only")
